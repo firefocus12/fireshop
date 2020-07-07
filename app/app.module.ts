826aedd63ad9725bfdc87c25f366bd7efaf4ab91
firefocus12 <61930079+firefocus12@users.noreply.github.com>
2020-07-07 19:41:28 +0530

    Update app.module.ts

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,15 +1,13 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { IonicModule } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { BarcodeScanner } from "@ionic-native/barcode-scanner/ngx";
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { PreviewPage } from './preview/preview.page';
 import { PlaceOrderPage } from './place-order/place-order.page';
 
 
@@ -27,6 +25,5 @@ import { PlaceOrderPage } from './place-order/place-order.page';
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent, PlaceOrderPage],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
